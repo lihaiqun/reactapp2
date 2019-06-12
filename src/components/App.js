ee228a0312400cec70ffb8c7e3b7bb48ebd8bb38
lihaiqun <1289181815@qq.com>
2019-06-12 11:25:26 +0800

    first commit reactapp2

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <div>hello react</div>
+//     )
+//   }
+// }
+
+// const App = function () {
+//   return (
+//     <div>hello react function</div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>hello react function1</div>
+//   )
+// }
+
+const App = () => (<div>hello react function2</div>)
+export default App;
